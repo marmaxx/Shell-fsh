d636b903d445cec3fa9310d24317f17f3203888e
veron <lucas.veron@etu.u-paris.fr>
2024-12-11 17:32:27 +0100

    modifications des fichiers de redirection

--- bin/fsh.c
+++ bin/fsh.c
@@ -18,26 +18,44 @@
 #include "../include/ftype.h"
 #include "../include/commande_structuree.h"
 #include "../include/if_else.h"
+#include "../include/redirection.h"
 
 int execute_commande_quelconque(char **args, int last_status, char *command){
-    // Quitte la boucle si le user ecrit exit 
+    /* Quitte la boucle si le user ecrit exit */ 
     if(strcmp(args[0], "exit") == 0){
         char *exit_arg; // initialisation de la val d'exit
-        if(args[1] != NULL) {
-            exit_arg = args[1];
-        } else {
+        if (args[1] ==  NULL){
             exit_arg = NULL;
         }
+        else{
+            if (args[2] != NULL){
+                fprintf(stderr, "exit: too many arguments\n");
+                last_status = -3;
+                exit_arg = NULL;
+                return last_status;
+            }
+            else{
+                exit_arg = args[1];
+            }
+        } 
         //printf("val de retour après exit : %d \n", func_exit(exit_arg,last_status));
         last_status = func_exit(exit_arg,last_status); // exit 
+        exit(last_status);
     }
-    // Gère le cas de la commande pwwd
+    /* Gère le cas de la commande pwwd */
     else if (strcmp(args[0], "pwd") == 0){
-        last_status = 0;
-        printf("%s\n", chemin_absolu());
+        // Vérifie si trop d'arguments sont fournis
+        if (args[1] != NULL) {
+            fprintf(stderr, "pwd: extra: invalid argument\n");
+            last_status = 1;
+        }
+        else{
+            last_status = 0;
+            fprintf(stdout, "%s\n", chemin_absolu());
+        }
     }
 
-    // Gère le cas de la commande for
+    /* Gère le cas de la commande for */ 
     else if (strcmp(args[0], "for") == 0){
         last_status = boucle_for_simple(args, last_status);
     }
@@ -50,7 +68,10 @@ int execute_commande_quelconque(char **args, int last_status, char *command){
         last_status = ftype(args);
     }
     else if(strcmp(args[0], "if") == 0){
-        last_status = executer_commande_if_else(args, last_status);
+        /*if (test(args[1])) last_status = commande_externe(args[2]);
+        else if (args[4] != NULL) last_status = commande_externe(args[4]);
+        */
+       last_status = executer_commande_if_else(args, last_status);
     }
     else{
         //printf("%s : commande invalide ou pas encore implémentée !\n", command);
@@ -66,37 +87,45 @@ int main(){
     rl_outstream = stderr;
 
     while(1){
-        // Création du prompt
+        /* Création du prompt */ 
         char prompt[PATH_MAX + 50];
         create_prompt(last_status, prompt, sizeof(prompt));
         fprintf(stderr, "%s", prompt);
         fflush(stderr);
 
-        // Lit la commande du user 
-        command = readline(""); 
+        /* Lit la commande du user */ 
+        command = readline("");
 
         if(command == NULL){
+            //printf("on sort de la boucle while");
             break;
         } 
 
+        /* Libere la memoire et passe a la prochaine iteration de la boucle */
         if (strlen(command) == 0) {
-            free(command);  // Libère la mémoire allouée pour une commande vide
-            continue;  // Passe à la prochaine itération de la boucle sans découper une commande vide
+            free(command); 
+            continue; 
         }
 
+        /* Ajouter la commande à l'historique */
         if (strlen(command) > 0) {
-            add_history(command);  // Ajouter la commande à l'historique
+            add_history(command);  
+        }
+        
+        /* Execution d'une redirection */  
+        if (is_redirection(command) == 0){
+            //printf("on a bien une redirection\n");
+            last_status = make_redirection(command,last_status);
         }
 
-        if (is_structured(command)){
-            int *result;
-            result = execute_structured_command(command, last_status);
-            last_status = result[1];
-            if (result[0] != 0) return last_status;
-            free(command);
+        /* Execution d'une commande structure */
+        else if (is_structured(command)){
+            //printf("c'est structuré ! \n");
+            last_status = *execute_structured_command(command, last_status);
         }
+
         else{
-            // decoupe la commande
+            /* Decoupe la commande */
             char **args = decoupe(command);
 
             /*printf("Affichage dans fsh: \n");
@@ -107,20 +136,26 @@ int main(){
             //printf("args[0] = %s et args[1] = %s\n", args[0], args[1]);
             printf("\n");*/
 
-            // Quitte la boucle si le user ecrit exit 
+            /* Quitte la boucle si le user ecrit exit */ 
             if(strcmp(args[0], "exit") == 0){
                 last_status = execute_commande_quelconque(args, last_status, command);
-                free(command);
-                free(args);
-                return last_status;
+                if (last_status != -3){
+                    free(command);
+                    free(args);
+                    return last_status;
+                }
+                last_status = 1;
             }
+            /* Execute la commande */
             else{
                 last_status = execute_commande_quelconque(args, last_status, command);
             }
-            free(command); 
+
+            /* clean la memeoire */ 
             /*for (int i = 0; args[i] != NULL; i++) {
                 free(args[i]);
             }*/
+            free(command);
             free(args);
         }
     }
