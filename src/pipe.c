1b1c0c5585abc6233fe8a43f25e944ea60d6066a
veron <lucas.veron@etu.u-paris.fr>
2024-12-12 17:39:34 +0100

    réglage d'erreurs dans le fichier pipe.c

--- src/pipe.c
+++ src/pipe.c
@@ -48,6 +48,7 @@ int verify_spaces_around_pipes(char *command) {
 }
 
 int decoupe_pipe_commande(char *command) {
+    
     if (!is_Pipe_Command(command)) {
         return 0;  //commande invalide si ce n'est pas une commande avec des pipes
     }
@@ -116,7 +117,6 @@ int decoupe_pipe_commande(char *command) {
 }
 
 int execute_pipe(char *command, int last_status) {
-    int result = last_status;
 
     int pipe_fd[args_count - 1][2]; //nombre de pipes = nombre de commandes - 1
 
@@ -132,15 +132,14 @@ int execute_pipe(char *command, int last_status) {
         pid_t pid = fork();
 
         if (pid == 0) { //processus enfant
-            //vérification des arguments avant exécution
+            
             //printf("Exécution de la commande: %s\n", args[i]); 
 
             //redirection d'entrée : si ce n'est pas le premier processus, lire depuis le pipe précédent
             if (i > 0) {
                 if (dup2(pipe_fd[i - 1][0], STDIN_FILENO) == -1) {
                     perror("dup2 input");
-                    result = 1;
-                    exit (1);
+                    _exit(1);
                 }
             }
 
@@ -148,8 +147,7 @@ int execute_pipe(char *command, int last_status) {
             if (i < args_count - 1) {
                 if (dup2(pipe_fd[i][1], STDOUT_FILENO) == -1) {
                     perror("dup2 output");
-                    result = 1;
-                    exit (1);
+                    _exit(1);
                 }
             }
 
@@ -162,34 +160,43 @@ int execute_pipe(char *command, int last_status) {
             //exécution de la commande
             if (!is_redirection(args[i])) { //si c'est une redirection, on gère la redirection
                 //fprintf(stderr, "olala");
-                if (result = make_redirection(args[i], last_status)) _exit (result);
+                _exit(make_redirection(args[i], last_status));
                 
             } else { //sinon, on exécute la commande "quelconque"
                 //fprintf(stderr, "On execute la commande : %s\n", args[i]);
                 char **args_i = decoupe(args[i]);
-                if (result = execute_commande_quelconque(args_i, last_status, command)) _exit(result);
+                _exit(execute_commande_quelconque(args_i, last_status, command));
+            
                 //printf("%i", result);
-            }
-            return result;
-
+            } 
         }
         
-
         else if (pid < 0){
             perror ("fork");
             return 1;
         }
     }
-
+    
     //fermeture des pipes dans le parent
     for (int i = 0; i < args_count-1; i++) {
         close(pipe_fd[i][0]);
         close(pipe_fd[i][1]);
     }
 
-    //on attend les processus enfants
+    int result = 0;
+
     for (int i = 0; i < args_count; i++) {
-        wait(NULL);
+        int status;
+        pid_t pid = wait(&status);
+        if (pid > 0) {
+            //on récupère la valeur de retour de l'enfant
+            if (WIFEXITED(status)) {
+                result = WEXITSTATUS(status); //on met à jour le résultat
+                //printf("%i\n", result);
+            } else {
+                result = 1; //si le processus enfant a échoué
+            }
+        }
     }
 
     //libération de la mémoire utilisée pour les arguments
@@ -197,5 +204,6 @@ int execute_pipe(char *command, int last_status) {
     args = NULL;
     args_count = 0;
 
+    //printf("%i\n", result);
     return result;
 }
