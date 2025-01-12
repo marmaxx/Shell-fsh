14fac2e9223420633e26975742c54a258aedfcf2
filaudea <marius.filaudeau@etu.u-paris.fr>
2025-01-12 18:56:57 +0100

    il manquait un argument a une fonction

--- include/boucle_for.h
+++ include/boucle_for.h
@@ -3,6 +3,6 @@
 
 int boucle_for (char ** args, int last_status);
 void concatenate_args(char *args[], char *result);
-int exec_interieur_for(struct dirent *entry, int last_status,int option_p, int option_r, int option_e, int option_t, int option_A, int size, char *ext, char *type, char *rep, char **commande, char **args);
+int exec_interieur_for(struct dirent *entry, char *name, int last_status,int option_p, int option_r, int option_e, int option_t, int option_A, int size, char *ext, char *type, char *rep, char **commande, char **args);
 
 #endif 
